0c9fd7410e5d7c524f946593e34998dac961d454
sejal <sejipat@gmail.com>
2026-02-12 08:04:46 +0530

    feat: implement authentication

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
+import cookieParser from cookie-parser;
+import authRoutes from "./routes/authRoutes.js"
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+//middlewares
 app.use(express.json());
+app.use(cookieParser());
+
+//routes
+app.use("/api/auth", authRoutes);
+
 
 connectDB().then(() => {
     app.listen(PORT, () => {

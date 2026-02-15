c1a4221c74617ec3af4a470195d474f6ae44ae1b
Anjali Phule <anjaliphule5@gmail.com>
2026-02-15 07:51:40 +0530

    add:sleepquiz backend

--- server/routes/dailyQuizRoutes.js
+++ server/routes/dailyQuizRoutes.js
@@ -4,8 +4,10 @@ import submitDailyQuiz from "../controllers/DailyQuizController.js"
 import authenticateToken  from "../middleware/authmiddleware.js";
 import submitAnxietyQuiz from "../controllers/AnxietyQuizController.js"
 import submitDepressionQuiz from "../controllers/DepressionQuizController.js";
+import submitSleepQuiz from "../controllers/SleepQuizController.js";
 
 router.post("/submit-dailyquiz",authenticateToken,  submitDailyQuiz)
 router.post("/submit-anxietyquiz", authenticateToken, submitAnxietyQuiz)
 router.post("/submit-depressionquiz", authenticateToken, submitDepressionQuiz)
+router.post("/submit-sleepquiz", authenticateToken, submitSleepQuiz)
 export default router 
